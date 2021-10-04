51ce151ada17524e58bc61d05d7c14e9a1ba8bde
Panzerxer <econ.evening2008@mail.ru>
2021-10-04 23:13:17 +0300

    veevev

---
+++ Course.java
@@ -0,0 +1,13 @@
+public class Course {
+int a;
+int b;
+int c;
+    public Course(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+}
+
+
+
---
+++ Player.java
@@ -0,0 +1,14 @@
+public class Player {
+    String name;
+    int age;
+    int IQ;
+
+    public Player(String name, int age, int IQ) {
+        this.name = name;
+        this.age = age;
+        this.IQ = IQ;
+        System.out.println("Персонаж " + name + " создан.");
+
+    }
+
+}

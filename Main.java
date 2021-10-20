236c3e079c4bc21b01e77d1405eb29a4cb5e0ebf
Panzerxer <econ.evening2008@mail.ru>
2021-10-20 19:39:10 +0300

    commit

--- Main.java
+++ Main.java
@@ -1,44 +1,46 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class main {
     public static void main(String[] args) {
-    Player player1 = new Player("Саня", 10, 60);
-    Player player2 = new Player("Толик", 22, 10);
-    Player player3 = new Player("Вася", 31, 92);
-    Player player4 = new Player("Игорь", 54, 130);
-   Team team = new Team("игруны", player1, player2, player3, player4); // создайте команду
-   team.sostav();
-        Course c = new Course(10,10, 50); //укажите сложность заданий
 
 
+        String[] list = new String[]{"word", "hello", "party", "peace", "Greace", "prototype", "elephant", "word", "batman", "silence", "party"};
+        ArrayList unicList = new ArrayList();
 
-        if(player1.IQ > c.a){
-            if(player1.IQ > c.b){
-                if(player1.IQ > c.c){
-                    System.out.println(player1.name + " Прошёл тесты");
-                } else {System.out.println(player1.name + " не прошёл тесты");}
-            } else {System.out.println(player1.name + " не прошёл тесты");}
-        } else {System.out.println(player1.name + " не прошёл тесты");}
-        if(player2.IQ > c.a){
-            if(player2.IQ > c.b){
-                if(player2.IQ > c.c){
-                    System.out.println(player2.name + " Прошёл тесты");
-                } else {System.out.println(player2.name + " не прошёл тесты");}
-            } else {System.out.println(player2.name + " не прошёл тесты");}
-        } else {System.out.println(player2.name + " не прошёл тесты");}
-        if(player3.IQ > c.a){
-            if(player3.IQ > c.b){
-                if(player3.IQ > c.c){
-                    System.out.println(player3.name + " Прошёл тесты");
-                }else {System.out.println(player3.name + " не прошёл тесты");}
-            }else {System.out.println(player3.name + " не прошёл тесты");}
-        }else {System.out.println(player3.name + " не прошёл тесты");}
-        if(player4.IQ > c.a){
-            if(player4.IQ > c.b){
-                if(player4.IQ > c.c){
-                    System.out.println(player4.name + " Прошёл тесты");
-                } else {System.out.println(player4.name + " не прошёл тесты");}
-            } else {System.out.println(player4.name + " не прошёл тесты");}
-        } else {System.out.println(player4.name + " не прошёл тесты");}
+//вывод уникальных слов
+        for (int i = 0; i < list.length; i++) {
+            if (!unicList.contains(list[i])){
+                unicList.add(list[i]);
+            }
+        }System.out.println(unicList);
+
+        //счет слов
+        for (int i = 0; i < unicList.size(); i++){
+            int count = 0;
+            for (int j = 0; j < list.length; j++){
+
+                if (list[j] == unicList.get(i)){
+                    count++;
+
+                }
+            }System.out.println(unicList.get(i) +" "+ count + "раз(а)");
+        }
+
+        ArrayList<Employee> phonebook = new ArrayList<>();
+        phonebook.add(new Employee(23232, "White"));
+        phonebook.add(new Employee(23233, "Green"));
+        phonebook.add(new Employee(23234, "Brown"));
+        phonebook.add(new Employee(23235, "Green"));
+
+
+        for(int i = 0; i < phonebook.size(); i++){
+            System.out.println(phonebook.get(i));
+        }
+
 
     }
 
 }
+
+
